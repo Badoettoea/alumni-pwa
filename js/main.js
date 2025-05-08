22bffec02ff81a6a59dacf2d13af90e0b1fd580f
badoettoea <87426546+Badoettoea@users.noreply.github.com>
2025-05-08 14:59:41 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 
+// Navigasi antar halaman
+function navigateTo(page) {
+    window.location.href = page;
+}
+
+// PWA Service Worker Registration
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js').then(registration => {
+            console.log('ServiceWorker registration successful');
+        }).catch(err => {
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
